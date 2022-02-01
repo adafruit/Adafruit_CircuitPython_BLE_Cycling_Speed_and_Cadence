89f76e0debb6057b37d7014238c59a90bc128b85
Alec Delaney <tekktrik@gmail.com>
2022-02-01 12:26:53 -0500

    Update type annotation for service argument in __init__()

--- adafruit_ble_cycling_speed_and_cadence.py
+++ adafruit_ble_cycling_speed_and_cadence.py
@@ -131,7 +131,7 @@ class CyclingSpeedAndCadenceService(Service):
         "Chain Ring",
     )
 
-    def __init__(self, service: Optional[Service] = None) -> None:
+    def __init__(self, service: Optional["CyclingSpeedAndCadenceService"] = None) -> None:
         super().__init__(service=service)
         # Defer creating buffer until we're definitely connected.
         self._measurement_buf = None
