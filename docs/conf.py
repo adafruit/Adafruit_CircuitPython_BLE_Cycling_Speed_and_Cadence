f47a8aeb4d9b3a4ae8b8b4cfa63af5e0afb2140d
Alec Delaney <tekktik@gmail.com>
2022-08-23 17:26:22 -0400

    Use year duration range for copyright attribution

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,14 @@ master_doc = "index"
 
 # General information about the project.
 project = "Adafruit BLE_Cycling_Speed_and_Cadence Library"
+creation_year = "2020"
 current_year = str(datetime.datetime.now().year)
-copyright = current_year + " Adafruit Industries"
+year_duration = (
+    current_year
+    if current_year == creation_year
+    else creation_year + " - " + current_year
+)
+copyright = year_duration + " Adafruit Industries"
 author = "Adafruit Industries"
 
 # The version info for the project you're documenting, acts as replacement for
